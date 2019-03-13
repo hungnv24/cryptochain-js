234c870cead07815280177ec514cac06a9706aa5
Hung NV <hungnv92@gmail.com>
2019-03-13 22:10:41 +0100

    Use binary format of hash to compute difficulty. Add Average mining time script to compare

---
+++ average-work.js
@@ -0,0 +1,30 @@
+const Blockchain = require('./blockchain')
+
+const blockchain = new Blockchain()
+
+blockchain.addBlock({ data: 'initial' })
+
+console.log('first block', blockchain.chain[blockchain.chain.length - 1])
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average
+
+const times = []
+
+let sumTimes = 0
+
+for (let i = 0; i < 100; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp
+
+  blockchain.addBlock({ data: `block ${i}` })
+
+  nextBlock = blockchain.chain[blockchain.chain.length - 1]
+  nextTimestamp = nextBlock.timestamp
+
+  timeDiff = nextTimestamp - prevTimestamp
+  times.push(timeDiff)
+
+  sumTimes += timeDiff
+  average = sumTimes / times.length
+
+  console.log(`Time to mine ${i}-th block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`)
+}
