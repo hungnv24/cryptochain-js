91f8d9aa333a6d656846511a65e3f7be1ef476cb
Hung NV <hungnv92@gmail.com>
2019-03-17 21:54:08 +0100

    Sync chain from root node after node started

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const Blockchain = require('./blockchain')
 const PubSub = require('./pubsub')
 
 const app = express()
 const blockchain = new Blockchain()
 const pubsub = new PubSub({ blockchain })
+
+const DEFAULT_PORT = 3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -24,7 +28,20 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks')
 })
 
-const DEFAULT_PORT = 3000
+const syncChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body)
+
+      console.log('sync and replace chain with ', rootChain)
+
+      blockchain.replaceChain(rootChain)
+    } else {
+      console.error(error)
+    }
+  })
+}
+
 let PEER_PORT
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -33,4 +50,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 
 const PORT = PEER_PORT || DEFAULT_PORT
 
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`))
+app.listen(PORT, () => { 
+  console.log(`listening at localhost:${PORT}`)
+  syncChains()
+})
